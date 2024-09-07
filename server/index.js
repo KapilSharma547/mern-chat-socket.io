03430fbfe05a36ec28074e81302fa7322a8091c8
Kapil Sharma <kapilsharmanfl97@gmail.com>
2024-09-07 22:12:09 +0530

    minor changes

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const io = socketIo(server, {
 
 
 io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+  // console.log(`User Connected: ${socket.id}`);
 
   socket.on("join_room", (data) => {
     socket.join(data);
